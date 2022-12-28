fde85591455a256b405da48e08cce663766ad40c
Illusion <godteen.peng@gmail.com>
2022-12-28 22:24:01 +0800

    Optimized test suite

--- tests/ClassParserTest.cpp
+++ tests/ClassParserTest.cpp
@@ -10,16 +10,27 @@ using namespace JVM;
 namespace unit_test {
 class ClassParserTest : public testing::Test {
   protected:
-  std::shared_ptr<ClassData> data;
-  std::shared_ptr<ClassFile> classFile;
+  static std::shared_ptr<ClassData> data;
+  static std::shared_ptr<ClassFile> classFile;
   static int pos;
+  // static const u4 MAGIC = 0xCAFEBABE;
+  // static const u2 MINOR_VERSION = 0x0;
+  // static const u2 MAJOR_VERSION = 0x34;
+  // static const u2 CONSTANT_POOL_COUNT = 0xA0;
+  // static const u2 ACCESS_FLAGS = 0x21;
+  // static const u2 THIS_CLASS_NAME_INDEX = 0x22;
+  // static const u2 SUPER_CLASS_NAME_INDEX = 0x35;
+  // static const u2 INTERFACE_COUNT = 0x0;
+  // static const u2 FIELDS_COUNT = 0x0C;
+  // static const u2 METHODS_COUNT = 0x03;
+  // static const u2 ATTRIBUTE_COUNT = 0x03;
   ClassParserTest() {
   }
 
   ~ClassParserTest() override {
-  }
+  } 
 
-  void SetUp() override {
+  static void SetUpTestSuite() {
     std::string classDir = "/home/android/jvm-demo/tests/javasample";
     DirClassReader reader(classDir);
     std::string className = "com.sample.Sample";
@@ -28,6 +39,11 @@ class ClassParserTest : public testing::Test {
     ASSERT_EQ(data->readErrno, SUCCEED);
     classFile = std::make_shared<ClassFile>();
     //ASSERT_TRUE(checkClassMagic(data->data));
+    pos = 0;
+  }
+
+  void SetUp() override {
+    
   }
   
 
@@ -35,12 +51,25 @@ class ClassParserTest : public testing::Test {
   }
 };
 
-int ClassParserTest::pos = 0;
+int ClassParserTest::pos;
+std::shared_ptr<ClassFile> ClassParserTest::classFile;
+std::shared_ptr<ClassData> ClassParserTest::data;
+const u4 MAGIC = 0xCAFEBABE;
+const u2 MINOR_VERSION = 0x0;
+const u2 MAJOR_VERSION = 0x34;
+const u2 CONSTANT_POOL_COUNT = 0xA0;
+const u2 ACCESS_FLAGS = 0x21;
+const u2 THIS_CLASS_NAME_INDEX = 0x22;
+const u2 SUPER_CLASS_NAME_INDEX = 0x23;
+const u2 INTERFACE_COUNT = 0x0;
+const u2 FIELDS_COUNT = 0x0C;
+const u2 METHODS_COUNT = 0x03;
+const u2 ATTRIBUTE_COUNT = 0x03;
 
 TEST_F(ClassParserTest, parseAndCheckMagic) {
   //int pos = 0;
   JVM::parseAndCheckMagic(data, classFile, pos);
-  ASSERT_EQ(classFile->magic, 0xCAFEBABE);
+  ASSERT_EQ(classFile->magic, MAGIC);
 }
 
 TEST_F(ClassParserTest, parseAndCheckVersion) {
@@ -48,8 +77,8 @@ TEST_F(ClassParserTest, parseAndCheckVersion) {
   LOG(INFO) << "parseAndCheckVersion current pos = " << pos;
   JVM::parseAndCheckVersion(data, classFile, pos);
   //ASSERT_EQ(pos, sizeof(classFile->magic) + sizeof(classFile->minorVersion) + sizeof(classFile->majorVersion));
-  ASSERT_EQ(classFile->minorVersion, 0x0);
-  ASSERT_EQ(classFile->majorVersion, 0x34);
+  ASSERT_EQ(classFile->minorVersion, MINOR_VERSION);
+  ASSERT_EQ(classFile->majorVersion, MAJOR_VERSION);
 }
 
 TEST_F(ClassParserTest, parseConstantPool) {
@@ -57,70 +86,61 @@ TEST_F(ClassParserTest, parseConstantPool) {
   LOG(INFO) << "parseConstantPool current pos = " << pos;
   JVM::parseConstantPool(data, classFile, pos);
   ASSERT_NE(classFile->constantPool->constantPoolCount, 0);
-  ASSERT_EQ(classFile->constantPool->constantPoolCount, 0xA0);
+  ASSERT_EQ(classFile->constantPool->constantPoolCount, CONSTANT_POOL_COUNT);
 }
 TEST_F(ClassParserTest, parseAccessFlags) {
   LOG(INFO) << "parseAccessFlags current pos = " << pos;
   JVM::parseAccessFlags(data, classFile, pos);
-  ASSERT_EQ(classFile->accessFlags, 0x21);
+  ASSERT_EQ(classFile->accessFlags, ACCESS_FLAGS);
 }
 TEST_F(ClassParserTest, parseThisClass) {
   LOG(INFO) << "parseThisClass current pos = " << pos;
   JVM::parseThisClass(data, classFile, pos);
-  ASSERT_EQ(classFile->thisClass, 0x22);
+  ASSERT_EQ(classFile->thisClass, THIS_CLASS_NAME_INDEX);
 }
 
 TEST_F(ClassParserTest, parseSuperClass) {
   LOG(INFO) << "parseSuperClass current pos = " << pos;
   JVM::parseSuperClass(data, classFile, pos);
-  ASSERT_EQ(classFile->superClass, 0x23);
+  ASSERT_EQ(classFile->superClass, SUPER_CLASS_NAME_INDEX);
 }
 
 TEST_F(ClassParserTest, parseInterfaces) {
   LOG(INFO) << "parseInterfaces current pos = " << pos;
   JVM::parseInterfaces(data, classFile, pos);
-  ASSERT_EQ(classFile->interfaces.size(), 0);
+  ASSERT_EQ(classFile->interfaces.size(), INTERFACE_COUNT);
 }
 
 TEST_F(ClassParserTest, parseFieldInfos) {
   LOG(INFO) << "parseFieldInfos current pos = " << pos << " superClass = ";
-  int mockPos = 8;
-  JVM::parseConstantPool(data, classFile, mockPos);
-  ASSERT_EQ(classFile->constantPool->constantPoolCount, 0xA0);
   JVM::parseFieldInfos(data, classFile, pos);
-  ASSERT_EQ(classFile->fields.size(), 0x0C);
+  ASSERT_EQ(classFile->fields.size(), FIELDS_COUNT);
 }
 TEST_F(ClassParserTest, parseMethodInfos) {
   LOG(INFO) << "parseMethodInfos current pos = " << pos << " superClass = ";
-  int mockPos = 8;
-  JVM::parseConstantPool(data, classFile, mockPos);
-  ASSERT_EQ(classFile->constantPool->constantPoolCount, 0xA0);
   JVM::parseMethodInfos(data, classFile, pos);
-  ASSERT_EQ(classFile->methods.size(), 0x03);
+  ASSERT_EQ(classFile->methods.size(), METHODS_COUNT);
 }
 TEST_F(ClassParserTest, parseAttributeInfos) {
   LOG(INFO) << "parseAttributeInfos current pos = " << pos << " superClass = ";
-  int mockPos = 8;
-  JVM::parseConstantPool(data, classFile, mockPos);
-  ASSERT_EQ(classFile->constantPool->constantPoolCount, 0xA0);
   JVM::parseAttributeInfos(data, classFile, pos);
-  ASSERT_EQ(classFile->attributes.size(), 0x03);
+  ASSERT_EQ(classFile->attributes.size(), ATTRIBUTE_COUNT);
 }
 
 TEST_F(ClassParserTest, parse) {
   pos = 0;
   classFile = JVM::parse(data);
-  ASSERT_EQ(classFile->magic, 0xCAFEBABE);
-  ASSERT_EQ(classFile->minorVersion, 0x0);
-  ASSERT_EQ(classFile->majorVersion, 0x34);
-  ASSERT_EQ(classFile->constantPool->constantPoolCount, 0xA0);
-  ASSERT_EQ(classFile->accessFlags, 0x21);
-  ASSERT_EQ(classFile->thisClass, 0x22);
-  ASSERT_EQ(classFile->superClass, 0x23);
-  ASSERT_EQ(classFile->interfaces.size(), 0);
-  ASSERT_EQ(classFile->fields.size(), 0x0C);
-  ASSERT_EQ(classFile->methods.size(), 0x03);
-  ASSERT_EQ(classFile->attributes.size(), 0x03);
+  ASSERT_EQ(classFile->magic, MAGIC);
+  ASSERT_EQ(classFile->minorVersion, MINOR_VERSION);
+  ASSERT_EQ(classFile->majorVersion, MAJOR_VERSION);
+  ASSERT_EQ(classFile->constantPool->constantPoolCount, CONSTANT_POOL_COUNT);
+  ASSERT_EQ(classFile->accessFlags, ACCESS_FLAGS);
+  ASSERT_EQ(classFile->thisClass, THIS_CLASS_NAME_INDEX);
+  ASSERT_EQ(classFile->superClass, SUPER_CLASS_NAME_INDEX);
+  ASSERT_EQ(classFile->interfaces.size(), INTERFACE_COUNT);
+  ASSERT_EQ(classFile->fields.size(), FIELDS_COUNT);
+  ASSERT_EQ(classFile->methods.size(), METHODS_COUNT);
+  ASSERT_EQ(classFile->attributes.size(), ATTRIBUTE_COUNT);
 }
 
 }
